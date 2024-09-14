dc8e5aa48d0ca92a74796235fcfb9b9eb2fa8f14
Chemmy <chengming0916@outlook.com>
2024-09-14 16:07:05 +0800

    添加爬虫示例

---
+++ SpiderSample/README.md
@@ -0,0 +1,19 @@
+# 网站爬虫示例
+
+基于BeautifulSoup4和requests的网站爬虫示例。
+
+## 使用方法
+
+1. 安装依赖：
+
+```bash
+pip install beautifulsoup4 requests
+```
+
+2. 运行脚本：
+
+```bash
+python main.py
+```
+
+
---
+++ SpiderSample/main.py
@@ -0,0 +1,49 @@
+import sys
+import time
+import requests
+from bs4 import BeautifulSoup
+
+headers = {
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
+}
+
+# 其实网页
+start_url = "https://www.bqgui.cc"
+
+novel_id = "1604"
+
+# 请求网页
+response = requests.get(start_url + "/book/" + novel_id, headers=headers)
+
+# 解析网页
+soup = BeautifulSoup(response.text, "html.parser")
+
+# 获取内容列表
+a_tags = soup.find("div", class_="listmain").find_all("a")
+
+novel_name = soup.find("div", class_="info").find("h1").text
+
+save_path = f"./{novel_name}.txt"
+
+chapters = {}
+
+with open(save_path, "w", encoding="utf-8") as f:
+    for a in a_tags:
+        try:
+            chapter_response = requests.get(
+                start_url + a.get("href"),
+                headers=headers,
+            )
+            chapter_soup = BeautifulSoup(chapter_response.text, "html.parser")
+            title = chapter_soup.find("h1", class_="wap_none").text
+            content = chapter_soup.find("div", id="chaptercontent").text
+
+            # 删除章节内容中的“请收藏本站”
+            endIndex = content.text.find("请收藏本站")
+            content = content[:endIndex]
+            
+            print("下载章节 - ", title)
+
+            f.write(title + "\n" + content + "\n")
+        except Exception as e:
+            print("下载章节失败 - ", a.get("href"))

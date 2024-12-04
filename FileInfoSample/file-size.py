9044fc9b973fae5df4694bb7153c28a0f7458276
Chemmy <chengming0916@outlook.com>
2024-12-04 17:34:39 +0800

    添加os.walk方式便利代码

--- FileInfoSample/file-size.py
+++ FileInfoSample/file-size.py
@@ -1,6 +1,15 @@
 import os
 
-dir_path = "F:\Music"
+dir_path = "E:\BaiduNetdiskDownload"
+
+
+def get_files_1(dir: str) -> list:
+    file_list = []
+
+    for home, dirs, files in os.walk(dir):
+        for file_name in files:
+            file_list.append(os.path.join(home, file_name))
+    return file_list
 
 
 def get_files(dir: str) -> list:
@@ -11,8 +20,7 @@ def get_files(dir: str) -> list:
 
     if os.path.isdir(dir):
         # for f in os.listdir(dir): # 简单
-        # for f in os.walk(dir):    # 生成目录树中的文件夹名和文件名
-        for f in os.scandir(dir):   # 高效
+        for f in os.scandir(dir):  # 高效
             if os.path.isdir(f):
                 file_list.append(get_files(f))
             file_list.append(os.path.join(dir, f))
@@ -31,7 +39,7 @@ def get_size_str(size: int) -> str:
 
 
 if __name__ == "__main__":
-    file_list = get_files(dir_path)
+    file_list = get_files_1(dir_path)
     for f in file_list:
         file_name = os.path.basename(f)
         file_size = os.path.getsize(f)
